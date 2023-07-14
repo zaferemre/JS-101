9268b8ebacd078c8cea43ac762ce351d28aa23a9
zaferemre <100843021+zaferemre@users.noreply.github.com>
2023-07-14 12:09:01 +0300

    3 Day study Update

---
+++ app.js
@@ -0,0 +1,135 @@
+const numbers = [20, 21, 22, 23, 24, 25, 26, 27];
+
+numbers.forEach(function (num) {
+  console.log(num);
+});
+
+function printTriple(n) {
+  console.log(n * 3);
+}
+
+numbers.forEach(printTriple);
+
+const books = [
+  {
+    title: "Good Omens",
+    authors: ["Terry Pratchett", "Neil Gaiman"],
+    rating: 4.25,
+  },
+  {
+    title: "Bone: The Complete Edition",
+    authors: ["Jeff Smith"],
+    rating: 4.42,
+  },
+  {
+    title: "American Gods",
+    authors: ["Neil Gaiman"],
+    rating: 4.11,
+  },
+  {
+    title: "A Gentleman in Moscow",
+    authors: ["Amor Towles"],
+    rating: 4.36,
+  },
+];
+
+books.forEach(function (book) {
+  console.log(book.title.toUpperCase());
+});
+
+for (let book of books) {
+  console.log(book.title.toUpperCase());
+}
+
+numbers.forEach(function (num, idx) {
+  console.log(idx, num);
+});
+
+//Map
+
+const words = ["Zafer", "elif", "luna", "fakir"];
+
+const doubles = numbers.map(function (num) {
+  return num * 2;
+});
+
+const numDetail = numbers.map(function (n) {
+  return {
+    value: n,
+    isEven: n % 2 == 0,
+  };
+});
+
+const abbrews = words.map(function (words) {
+  return words.toUpperCase().split("").join(".");
+});
+
+const bookNames = books.map(function (book) {
+  return book.title;
+});
+
+//Arrow functions
+
+const squareFunc = function (x) {
+  return x * x;
+};
+const square = (x) => {
+  return x * x;
+};
+
+//Parantez ve no coma
+const squareNoReturn = (x) => x * x;
+
+const squareOneLine = (x) => x * x;
+
+const doubles1 = numbers.map((n) => n * 2);
+const doubles2 = numbers.map((n) => {
+  return n * 2;
+});
+
+//.find
+let movies = [
+  "The Fantastic Mr. Fox",
+  "Mr.and Mrs. Smith",
+  "Mrs. Doubtfire",
+  "Mr. Deeds",
+];
+
+const movie1 = movies.find((movie) => {
+  return movie.includes("Mrs");
+});
+
+const movie2 = movies.find((movie) => movie.indexOf("Mrs") === 0);
+
+// .filter
+const oddNumbersArr = numbers.filter((num) => num % 2 === 1);
+
+// .every .some returns bool
+const moviesEvery = movies.some((word) => word.startsWith("M"));
+
+//integer sort
+//Descending
+const dscDort = numbers.slice().sort((a, b) => b - a);
+
+books.sort((a, b) => a.rating - b.rating); //This orders the array into a sorted by increasing ratings
+
+//greeting is predetermined if it is not set while calling
+//Default parameter
+const greet = (person, greeting = "HI!") =>
+  console.log(`${greeting} , ${person}`);
+
+// If you want to address each element of an array or any object you can use ...array
+// As it will reach to the elements each (not the whole array as a block)
+
+const combinedExample = [...numbers, ...bookNames];
+
+//This is useful since if you make them equal to eachother an other way
+//They will address the same so any change in the main array changes the other array
+const numbersCopy = [...numbers];
+
+const newInfoBook = { ...books[0], myReview: 4.9 };
+// Keep in mind this doesnt changes the books array rather only creates a copy for the
+//Adrassed element
+
+//REST
+const sumOfUnknownAmountNums = (...nums) => nums.reduce((acc, x) => acc + x);
--- main.js
+++ main.js
@@ -1,12 +1,70 @@
-function factorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+const canvasHeight = 400;
+const canvasWidth = 720;
+
+const mazeWall = {
+    name: "wall",
+    color: 0,
+    value: -1,
+    height: 10,
+};
+
+const mazePrize = {
+    name: "prize",
+    color: "255, 171, 25",
+    value: "prize",
+    height: 10,
+};
+
+const startPoint = {
+    name: "start",
+    color: 255,
+    value: -1,
+    height: 10,
+};
+
+const mazeArray = [];
+
+for (let i = 0; i < 72; i++) {
+    const row = [];
+    for (let j = 0; j < 40; j++) {
+        row.push(mazeWall.name);
+    }
+    mazeArray.push(row);
+}
+console.log(mazeArray);
+console.log(mazeArray[25][25]);
+
+function setup() {
+    frameRate(30);
+    textSize(30);
+    textAlign(CENTER);
+    createCanvas(canvasWidth, canvasHeight);
 }
 
-// Example usage of the factorial function
-const number = 5;
-const result = factorial(number);
-console.log(`The factorial of ${number} is: ${result}`);
+//Draws the initial grid of the table
+function draw() {
+    drawMaze();
+    drawTable();
+}
+
+function drawTable() {
+    background(0, 212, 178);
+    noStroke();
+    for (let i = 0; i < canvasHeight; i += 10) {
+        for (let j = 0; j < canvasWidth; j += 10) {
+            if (i % 20 == 0) {
+                rect(j * 2, i, 10);
+                fill(0, 140, 157);
+            } else {
+                rect(j * 2 + 10, i, 10);
+                fill(0, 140, 157);
+            }
+        }
+    }
+}
+
+function drawMaze() {
+    noStroke();
+    rect(0, 0, 10);
+    fill(255);
+}
