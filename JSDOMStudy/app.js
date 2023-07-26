380dbe4919131caac165e683af0646452c769fad
zaferemre <100843021+zaferemre@users.noreply.github.com>
2023-07-26 12:36:35 +0300

    Up to Async Await study

--- JSDOMStudy/app.js
+++ JSDOMStudy/app.js
@@ -1,50 +1,37 @@
-// To select the element with id of bear-photo:
-document.getElementById("bear-photo");
-
-//To select the element with id of main:
-document.getElementById("main");
-
-//Promise Study
-
-const makeDogPromise = () => {
-  return new Promise((resolved, rejected) => {
-    setTimeout(() => {
-      const rand = Math.random();
-      if (rand < 0.5) {
-        resolved();
-      } else {
-        rejected();
-      }
-    }, 1000);
-  });
-};
-
-makeDogPromise()
-  .then(() => {
-    console.log("Onaylandı");
-  })
-  .catch(() => {
-    console.log("Reddedildi");
-  });
-
-const fakeRequest = (url) => {
+//This is a FAKE Http Request Function
+//It takes 1 second to resolve or reject the promise, depending on the url that is passed in
+/* const fakeRequest = (url) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const rand = Math.random();
-
       const pages = {
         "/users": [
           { id: 1, username: "Bilbo" },
-          { id: 2, username: "Frodo" },
+          { id: 5, username: "Esmerelda" },
         ],
+        "/users/1": {
+          id: 1,
+          username: "Bilbo",
+          upvotes: 360,
+          city: "Lisbon",
+          topPostId: 454321,
+        },
+        "/users/5": {
+          id: 5,
+          username: "Esmerelda",
+          upvotes: 571,
+          city: "Honolulu",
+        },
+        "/posts/454321": {
+          id: 454321,
+          title: "Ladies & Gentlemen, may I introduce my pet pig, Hamlet",
+        },
         "/about": "This is the about page!",
       };
       const data = pages[url];
-
       if (data) {
-        resolve({ status: 200, data });
+        resolve({ status: 200, data }); //resolve with a value!
       } else {
-        reject({ status: 404 });
+        reject({ status: 404 }); //reject with a value!
       }
     }, 1000);
   });
@@ -52,11 +39,215 @@ const fakeRequest = (url) => {
 
 fakeRequest("/users")
   .then((res) => {
-    console.log("Status Code", res.status);
-    console.log("Status Code", res.data);
-    console.log("RequestWorked");
+    console.log(res);
+    const id = res.data[0].id;
+    return fakeRequest(`/users/${id}`);
   })
-  .catch((res) => {
-    console.log(res.status);
-    console.log("ReqFailed");
+  .then((res) => {
+    console.log(res);
+    const postId = res.data.topPostId;
+    return fakeRequest(`/posts/${postId}`);
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log("OH NO!", err);
   });
+ */
+// ************************************************
+// ATTEMPT 2 (deliberate error to illustrate CATCH)
+// ************************************************
+// fakeRequest('/users')
+// 	.then((res) => {
+// 		console.log(res);
+// 		const id = res.data[0].id;
+// 		return fakeRequest(`/useALSKDJrs/${id}`); //INVALID URL, CATCH WILL RUN!
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 		const postId = res.data.topPostId;
+// 		return fakeRequest(`/posts/${postId}`);
+// 	})
+// 	.then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		console.log('OH NO!', err);
+// 	});
+
+//HTTP REQUESTS COURSE
+/* 
+const swapiResponse = {
+  name: "Yavin IV",
+  rotation_period: "24",
+  orbital_period: "4818",
+  diameter: "10200",
+  climate: "temperate, tropical",
+  gravity: "1 standard",
+  terrain: "jungle, rainforests",
+  surface_water: "8",
+  population: "1000",
+  residents: [],
+  films: ["https://swapi.dev/api/films/1/"],
+  created: "2014-12-10T11:37:19.144000Z",
+  edited: "2014-12-20T20:58:18.421000Z",
+  url: "https://swapi.dev/api/planets/3/",
+};
+
+const firstReq = new XMLHttpRequest();
+
+firstReq.addEventListener("load", () => {
+  console.log("FIRST REQUEST SENT");
+  const data = JSON.parse(firstReq.responseText);
+  console.log(data); */
+//console.log(this.responseText);
+//if this is wanted to use you should use a function instead
+//of an arrow function
+
+/*   const filmURL = data.results[0].films[0];
+  const filmReq = new XMLHttpRequest();
+  filmReq.addEventListener("load", function () {
+    console.log("SECOND REQUEST SENT");
+    const filmData = JSON.parse(this.responseText);
+    console.log(filmData);
+    console.log(filmData.title);
+  });
+  filmReq.addEventListener("error", function () {
+    console.log("ERROR", e);
+  });
+  filmReq.open("GET", filmURL);
+  filmReq.send();
+});
+
+firstReq.addEventListener("error", () => {
+  console.log("Error");
+});
+
+firstReq.open("GET", "https://swapi.dev/api/planets/");
+firstReq.send();
+console.log("Request Sent!"); */
+
+//HTTPRequest with Fetch
+//returns a promise
+
+//Response returns a readable stream which is a byte data
+//Which allows for extremely large things to be read as you go
+//.json reads it to completion
+
+/* const prom = fetch("https://swapi.dev/api/planets/")
+  .then((response) => {
+    console.log(response);
+    //  console.log(response.json()); //This takes a long time and returns a promise
+
+    if (!response.ok) {
+      //This allows for the code to not run if 404
+      throw new Error(`Status Code Error: ${response.status}`);
+    } else {
+      response.json().then((data) => {
+        //This prints the promise
+        console.log("**************FETCH**************");
+        console.log(data);
+
+        for (let planet of data.results) {
+          console.log(planet.name);
+        }
+      });
+    }
+  })
+  .catch((err) => {
+    console.log("Something is wrong");
+    console.log(err);
+  });
+ */
+
+//Then chaining
+/* const checkStatusAndParse = (response) => {
+  if (!response.ok) throw Error(`Status Code Error: ${response.status}`);
+  return response.json();
+};
+
+fetch("https://swapi.dev/api/planets/")
+  .then(checkStatusAndParse)
+  .then((data) => {
+    console.log("FETCHED ALL PLANETS (first 10)");
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+    const nextURL = data.next;
+    return fetch(nextURL);
+  })
+  .then(checkStatusAndParse)
+  .then((data) => {
+    console.log("Fetched Next 10 Planets");
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+  })
+  .catch((err) => {
+    console.log("Something is wrong");
+    console.log(err);
+  });
+ */
+
+//Axios
+
+axios
+  .get("https://swapi.dev/api/planets/")
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Async returns a promise
+async function greet() {
+  return "Selam";
+}
+
+greet().then((val) => {
+  console.log("Promise resolved", val);
+});
+
+async function add(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw "X and Y must be numbers";
+  }
+  return x + y;
+}
+
+add("e", "r")
+  .then((val) => {
+    console.log("Promise resolved", val);
+  })
+  .catch((err) => {
+    console.log("Promise rejected", err);
+  });
+
+/* function getPlanets() {
+  return axios.get("https://swapi.dev/api/planets/");
+}
+getPlanets().then((res) => {
+  console.log(res.data);
+});
+ */
+
+async function getPlanets() {
+  const res = await axios.get("https://swapi.dev/api/planets/");
+  console.log(res.data);
+}
+getPlanets().catch((err) => {
+  console.log("In CATCH!! ");
+  console.log(err);
+});
+
+async function getPlanetsTryCatch() {
+  try {
+    const res = await axios.get("https://swapi.dev/api/planets/");
+    console.log(res.data);
+  } catch (e) {
+    console.log("In CATCH!! ");
+    console.log(err);
+  }
+}
